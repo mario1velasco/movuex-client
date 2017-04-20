e22e217bfe2bf0a401aff692aa307ea17ba1da53
Andrea Zucchini <azga84@gmail.com>
2017-04-21 00:31:09 +0200

    level bar component

--- src/components/LevelBar/LevelBar.vue
+++ src/components/LevelBar/LevelBar.vue
@@ -15,44 +15,45 @@
 </template>
 
 <script>
-import Breadcrumb from 'vue-bulma-breadcrumb'
+  import Breadcrumb from 'vue-bulma-breadcrumb'
 
-export default {
-  components: {
-    Breadcrumb
-  },
+  export default {
+    name: 'level-bar',
+    components: {
+      Breadcrumb
+    },
 
-  data () {
-    return {
-      list: null
-    }
-  },
-
-  created () {
-    this.getList()
-  },
-
-  computed: {
-    name () {
-      return this.$route.name
-    }
-  },
-
-  methods: {
-    getList () {
-      let matched = this.$route.matched.filter(item => item.name)
-      const first = matched[0]
-      if (first && (first.name !== 'Dashboard' || first.path !== '')) {
-        matched = [{ name: 'Dashboard', path: '/' }].concat(matched)
+    data () {
+      return {
+        list: null
       }
-      this.list = matched
-    }
-  },
+    },
 
-  watch: {
-    $route () {
+    created () {
       this.getList()
+    },
+
+    computed: {
+      name () {
+        return this.$route.name
+      }
+    },
+
+    methods: {
+      getList () {
+        let matched = this.$route.matched.filter(item => item.name)
+        const first = matched[0]
+        if (first && (first.name !== 'Dashboard' || first.path !== '')) {
+          matched = [{name: 'Dashboard', path: '/'}].concat(matched)
+        }
+        this.list = matched
+      }
+    },
+
+    watch: {
+      $route () {
+        this.getList()
+      }
     }
   }
-}
 </script>
