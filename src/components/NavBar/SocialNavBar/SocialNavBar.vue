06ea99d7924c4a05a78262dd031f10bae0e87170
Andrea Zucchini <azga84@gmail.com>
2017-04-21 00:32:29 +0200

    social nav bar component

---
+++ src/components/NavBar/SocialNavBar/SocialNavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>
+    <span class="icon">
+      <i class="fa fa-code"></i>
+    </span> with
+    <span class="icon">
+        <i class="fa fa-heart"></i>
+    </span> by
+    <a href="https://github.com/zucchinidev" target="_blank">Zucchinidev</a>
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'social-nav-bar'
+  }
+</script>
+<style scoped>
+  p {
+    padding: 10px;
+  }
+</style>
