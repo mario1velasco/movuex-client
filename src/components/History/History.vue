3324e82a282a333eaacc4e472b3c796b1bbce9b5
Andrea Zucchini <azga84@gmail.com>
2017-04-21 00:34:24 +0200

    History component

---
+++ src/components/History/History.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="history">
+    <div class="history-list" v-if="filmsWithNotes">
+
+    </div>
+    <template v-else>
+      <i class="fa fa-sticky-note-o empty-notes" aria-hidden="true"></i>
+      <p class="no-results">
+        Ninguna nota añadida
+      </p>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'history',
+    data () {
+      return {
+        filmsWithNotes: null
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" src="./History.scss" scoped></style>
