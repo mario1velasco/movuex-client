cd5c0436844492dae7b7a57f445eeb8d969f7f6e
Andrea Zucchini <azga84@gmail.com>
2017-04-21 00:30:55 +0200

    create footer component

--- src/components/FooterBar/FooterBar.vue
+++ src/components/FooterBar/FooterBar.vue
@@ -2,20 +2,7 @@
   <footer class="footer">
     <div class="container">
       <div class="content has-text-centered">
-        <p class="social">
-          <a :href="'https://github.com/' + repository">
-            <span class="icon">
-              <i class="fa fa-github"></i>
-            </span>
-          </a>
-          <a href="https://twitter.com/_fundon">
-            <span class="icon">
-              <i class="fa fa-twitter"></i>
-            </span>
-          </a>
-        </p>
-        <p><span class="icon"><i class="fa fa-code"></i></span> with <span class="icon"><i class="fa fa-heart"></i></span> by <a href="https://github.com/fundon">fundon</a>.</p>
-        <p>Code licensed under <a :href="'https://github.com/' + repository + '/blob/master/LICENSE'">{{ license }}</a>.</p>
+
       </div>
     </div>
   </footer>
@@ -23,30 +10,8 @@
 
 <script>
 export default {
-
-  data () {
-    return this.$store.state.pkg
-  }
-
+  name: 'footer-bar'
 }
 </script>
 
-<style lang="scss">
-@import '~bulma/sass/utilities/mixins';
-
-.footer {
-  margin-left: 180px;
-  
-  @include mobile() {
-    margin-left: 0;
-  }
-  
-  .social a {
-    border-bottom: none !important;
-  }
-
-  .fa.fa-heart {
-    color: red;
-  }
-}
-</style>
+<style lang="scss" src="./FooterBar.scss"></style>
