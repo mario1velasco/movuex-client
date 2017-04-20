6148c7abbc8d0093ce2f8e18a76d1eb99eaa347e
Andrea Zucchini <azga84@gmail.com>
2017-04-21 00:33:33 +0200

    social sidebar component

---
+++ src/components/Sidebar/SocialSidebar/SocialSidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="social">
+    <a href="https://github.com/zucchinidev" target="_blank">
+      <span class="icon">
+        <i class="fa fa-github"></i>
+      </span>
+    </a>
+    <a href="https://twitter.com/zucchinidev" target="_blank">
+      <span class="icon">
+        <i class="fa fa-twitter"></i>
+      </span>
+    </a>
+    <div>
+      <a href="https://github.com/zucchinidev" target="_blank">Licencia</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'social-sidebar'
+  }
+</script>
+<style lang="scss" scoped>
+  .social {
+    position: absolute;
+    bottom: 1vh;
+    width: 100%;
+  }
+</style>
