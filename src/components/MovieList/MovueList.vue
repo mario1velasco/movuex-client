a7321ed4fca4bcbf4c3c201cbbfd8191bb68c2a0
Andrea Zucchini <azga84@gmail.com>
2017-04-21 00:31:25 +0200

    create movie list component

---
+++ src/components/MovieList/MovueList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="movie-list">
+    <ul v-if="films">
+      <li v-for="film in films">
+        <transition appear name="fade">
+          <article>
+            <figure>
+              <img v-cloak :src="film.Poster" :alt="film.Title">
+              <figcaption>
+                {{ film.Title }} {{ film.Year }}
+              </figcaption>
+            </figure>
+          </article>
+        </transition>
+      </li>
+    </ul>
+    <p class="no-results" v-else>
+      No hay resultados
+    </p>
+  </div>
+</template>
+
+<script>
+  import mocks from '../../mock/films'
+  export default {
+    name: 'MovieList',
+    data () {
+      return {
+        films: mocks.Search
+      }
+    }
+  }
+</script>
+
+<style lang='scss' src="./MovieList.scss"></style>
