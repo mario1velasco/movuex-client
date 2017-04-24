6b216d4cb051f65c473e3c02e2c6dc97b297157d
Andrea Zucchini <azga84@gmail.com>
2017-04-24 23:39:49 +0200

    add child routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '../components/Dashboard/Dashboard'
+import NotFound from '../components/NotFound/NotFound'
+import Movie from '../components/Movie/Movie'
+import MovieList from '../components/MovieList/MovieList'
+import GoToDashboard from '../components/GoToDashboard/GoToDashboard'
 
 Vue.use(Router)
 
@@ -8,8 +12,43 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Dashboard',
-      component: Dashboard
+      component: Dashboard,
+      children: [
+        {
+          path: '',
+          name: 'movieList',
+          component: MovieList,
+          props: route => {
+            const { query } = route
+            return query
+          }
+        },
+        {
+          path: '/film/:film',
+          name: 'movie',
+          components: {
+            default: Movie,
+            action: GoToDashboard
+          },
+          props: {
+            default: true, // inject film as property
+            action: route => {
+              console.log(route)
+            }
+          }
+        }
+      ]
+    },
+    {
+      path: '/not-found',
+      name: '404',
+      component: NotFound
+    },
+    {
+      path: '*',
+      redirect: {
+        name: '404'
+      }
     }
   ]
 })
