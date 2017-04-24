cf070a6a93df60ffa4674364a977c21b368e30d7
Andrea Zucchini <azga84@gmail.com>
2017-04-24 23:36:42 +0200

    subscribe to search event

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
-    <search></search>
-    <movie-list></movie-list>
+    <search @search="onSearch"></search>
+    <router-view name="action"></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import MovieList from '../MovieList/MovueList'
+  import MovieList from '../MovieList/MovieList'
   import Search from '../Search/Search'
+  import mocks from '../../mock/films'
   export default {
     name: 'dashboard',
     components: {
       Search,
       MovieList
+    },
+    data () {
+      return {
+        films: mocks.Search
+      }
+    },
+    methods: {
+      onSearch (criteria) {
+        this.$router.push({
+          name: 'movieList',
+          query: {
+            criteria
+          }
+        })
+      }
     }
   }
 </script>
