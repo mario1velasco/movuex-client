ae376a9963dccf4c448f160164f4d3a45297f6b4
Andrea Zucchini <azga84@gmail.com>
2017-04-24 23:40:43 +0200

    not found route

---
+++ src/components/NotFound/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="not-found">
+    <h1>
+      Not Found
+    </h1>
+
+    <p>
+      Woops! Looks like this page doesn't exist
+    </p>
+
+    <router-link to='{ name: "movieList" }'>
+      Go to index!
+    </router-link>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'NotFound'
+  }
+</script>
+
+<style lang='scss' src='./NotFound.scss' scoped></style>
