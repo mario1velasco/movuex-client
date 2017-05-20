7b445c63fd1c972a7430b81cb310eca77a8397ca
Andrea Zucchini <azga84@gmail.com>
2017-05-20 23:22:31 +0200

    sho social nav component

---
+++ src/components/Show/ShowSocialNav/ShowSocialNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="level is-mobile">
+    <div class="level-left">
+      <a class="level-item">
+        <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+      </a>
+      <a class="level-item">
+        <span class="icon is-small"><i class="fa fa-heart"></i></span>
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'show-social-nav',
+    props: {
+      show: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style lang='scss' src="./ShowSocialNav.scss"></style>
