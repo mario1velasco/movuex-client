f992ba90e869c36787dd33d24026c12117740008
Andrea Zucchini <azga84@gmail.com>
2017-05-20 23:21:35 +0200

    show figure component

---
+++ src/components/Show/ShowFigure/ShowFigure.vue
@@ -0,0 +1,25 @@
+<template>
+  <figure class="image is-128x128">
+    <img v-if='show.image && show.image.original'
+         :src="show.image.original"
+         :alt="`${show.name} poster`" class="poster">
+  </figure>
+</template>
+
+<script>
+  export default {
+    name: 'show-figure',
+    props: {
+      show: {
+        type: Object
+      }
+    },
+    methods: {
+      getGenres (genres) {
+        return genres.join(', ')
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped src="./ShowFigure.scss"></style>
