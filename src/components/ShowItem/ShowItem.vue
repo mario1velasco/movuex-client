ba9c91969064c3c37c6f3b4a15e64b83d59371fe
Andrea Zucchini <azga84@gmail.com>
2017-05-20 15:26:20 +0200

    create show item component

--- src/components/ShowItem/ShowItem.vue
+++ src/components/ShowItem/ShowItem.vue
@@ -1,36 +1,19 @@
 <template>
-  <div class="card" v-if='show'>
-    <div class="card-image">
-      <figure class="image is-4by3">
-        <img v-if='show.image && show.image.medium'
-             :src="show.image.medium"
-             :alt="`${show.name} poster`" class="poster">
-      </figure>
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img v-if='show.image && show.image.medium'
-                 :src="show.image.medium"
-                 :alt="`${show.name} poster`" class="poster">
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">{{ show.name }}</p>
-          <p class="subtitle is-6"> {{ show.premiered }}</p>
-        </div>
-      </div>
-    </div>
-    <div v-if="!show">
-      Show no encontrado
-    </div>
-  </div>
+  <article>
+    <figure>
+      <router-link :to="{name: 'show', params: {showId: show.id}}">
+        <img v-cloak :src="show.image.medium" :alt="show.name">
+      </router-link>
+      <figcaption>
+        {{ show.name }} - {{ show.premiered }}
+      </figcaption>
+    </figure>
+  </article>
 </template>
 
 <script>
   export default {
-    name: 'show-card',
+    name: 'show-item',
     props: {
       show: {
         type: Object
@@ -39,4 +22,4 @@
   }
 </script>
 
-<style lang='scss' src="./ShowCard.scss"></style>
+<style lang='scss' src="./ShowItem.scss"></style>
