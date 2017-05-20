b4f24cf82356c68d2ab694b284f22b1dcf6f4fbb
Andrea Zucchini <azga84@gmail.com>
2017-05-20 13:25:14 +0200

    change child component

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '../components/Dashboard/Dashboard'
 import NotFound from '../components/NotFound/NotFound'
-import Movie from '../components/Movie/Movie'
-import MovieList from '../components/MovieList/MovieList'
+import ShowContainer from '../containers/ShowContainer'
+import ShowListContainer from '../containers/ShowListContainer'
 import GoToDashboard from '../components/GoToDashboard/GoToDashboard'
 
 Vue.use(Router)
@@ -16,22 +16,22 @@ export default new Router({
       children: [
         {
           path: '',
-          name: 'movieList',
-          component: MovieList,
+          name: 'showList',
+          component: ShowListContainer,
           props: route => {
             const { query } = route
             return query
           }
         },
         {
-          path: '/film/:film',
-          name: 'movie',
+          path: '/show/:showId',
+          name: 'show',
           components: {
-            default: Movie,
+            default: ShowContainer,
             action: GoToDashboard
           },
           props: {
-            default: true, // inject film as property
+            default: true, // inject show as property
             action: route => {
               console.log(route)
             }
