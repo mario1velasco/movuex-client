2fddce79623a1d1f178750cfad30eff389a202f9
Andrea Zucchini <azga84@gmail.com>
2017-05-20 23:22:43 +0200

    use new components

--- src/components/Show/Show.vue
+++ src/components/Show/Show.vue
@@ -1,71 +1,38 @@
 <template>
-
   <div class="box">
     <article class="media">
       <div class="media-left">
-        <figure class="image is-128x128">
-          <img v-if='show.image && show.image.original'
-               :src="show.image.original"
-               :alt="`${show.name} poster`" class="poster">
-        </figure>
+        <show-figure :show="show"></show-figure>
       </div>
       <div class="media-content">
-        <div class="content">
-          <h3>{{show.name}}</h3>
-          <div class="sumary" v-html="show.summary"></div>
-          <p>
-            <small>Genres: {{ getGenres(show.genres) }}</small>
-          </p>
-          <p>
-            <small>Airs on: {{ show.network.country.name }} - {{ show.network.name }} </small>
-          </p>
-          <p>
-            <small>Schedule: {{ show.schedule.days[0] }} - {{ show.schedule.time }} </small>
-          </p>
-          <p>
-            <small>Status: {{ show.status }} </small>
-          </p>
-          <p>
-            <small>Show type: {{ show.type }} </small>
-          </p>
-        </div>
-        <nav class="level is-mobile">
-          <div class="level-left">
-            <a class="level-item">
-              <span class="icon is-small"><i class="fa fa-reply"></i></span>
-            </a>
-            <a class="level-item">
-              <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-            </a>
-            <a class="level-item">
-              <span class="icon is-small"><i class="fa fa-heart"></i></span>
-            </a>
-          </div>
-        </nav>
+        <show-content :show="show"></show-content>
       </div>
     </article>
-    <div v-if="!show" class="column">
-      Show no encontrado
-    </div>
+    <show-not-found v-if="!show"></show-not-found>
   </div>
 </template>
 
 <script>
   import ShowCard from '../ShowItem/ShowItem'
+  import ShowSocialNav from './ShowSocialNav/ShowSocialNav'
+  import ShowContent from './ShowContent/ShowContent'
+  import ShowFigure from './ShowFigure/ShowFigure'
+  import ShowNotFound from './ShowNotFound/ShowNotFound'
   export default {
-    components: {ShowCard},
+    components: {
+      ShowNotFound,
+      ShowFigure,
+      ShowContent,
+      ShowSocialNav,
+      ShowCard
+    },
     name: 'Show',
     props: {
       show: {
         type: Object
       }
-    },
-    methods: {
-      getGenres (genres) {
-        return genres.join(', ')
-      }
     }
   }
 </script>
 
-<style lang='scss' src="./Show.scss"></style>
+<style lang='scss' scoped src="./Show.scss"></style>
