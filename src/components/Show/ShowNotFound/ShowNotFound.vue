c90358b68f08a7900d8988322595d87228ab383d
Andrea Zucchini <azga84@gmail.com>
2017-05-20 23:21:53 +0200

    show not found component

---
+++ src/components/Show/ShowNotFound/ShowNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="column">
+    Show no encontrado
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'show-not-found',
+    props: {
+      show: {
+        type: Object
+      }
+    }
+  }
+</script>
+
+<style lang='scss' src="./ShowNotFound.scss"></style>
