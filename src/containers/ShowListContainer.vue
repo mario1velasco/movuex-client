b21da0e48c98e61f7fcd85e54f2d403ea1ef3355
Andrea Zucchini <azga84@gmail.com>
2017-05-20 13:21:10 +0200

    change names

---
+++ src/containers/ShowListContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <show
+    :show="show"
+    v-if="show"
+  ></show>
+</template>
+
+<script>
+  import Show from '../components/Show/Show'
+  import { TVMazeApiService } from '../services'
+  export default {
+    components: {Show},
+    name: 'ShowContainer',
+    props: {
+      showId: {
+        type: String,
+        require: true
+      }
+    },
+    data () {
+      return {
+        show: null,
+        service: null
+      }
+    },
+    created () {
+      this.service = new TVMazeApiService()
+      this.service.getShow(this.showId).then(response => {
+        this.show = response
+      })
+    }
+  }
+</script>
+
