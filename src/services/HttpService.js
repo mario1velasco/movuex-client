7bf0a4eaa113c0a2f92131d336ffee2e8700578a
Andrea Zucchini <azga84@gmail.com>
2017-05-29 00:03:24 +0200

    http service

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,14 +1,43 @@
+import axios from 'axios'
+
 export class HttpService {
-  static getBaseUrl () {
-    return 'http://www.omdbapi.com/?s='
+  static all (requests) {
+    return axios.all(requests).then(axios.spread((...args) => {
+      return args.map(value => {
+        const {data, config} = value
+        return {
+          data,
+          config
+        }
+      })
+    }))
   }
 
-  static get (url) {
-    return window.fetch(url)
-      .then(HttpService.getJSON)
+  static get (url, params, config) {
+    return axios
+      .get(url, {params, ...HttpService.getConfiguration(config)})
+      .then(HttpService.format)
   }
 
-  static getJSON (response) {
-    return response.json()
+  static post (url, params, config) {
+    params = HttpService.getMergedParams(params)
+    return axios
+      .post(url, params, config)
+      .then(HttpService.format)
+  }
+
+  static getConfiguration (config) {
+    return {
+      ...config
+      // add here the default configuration, interceptors, etc
+      // the config object will be overwritten!!!
+    }
+  }
+
+  static format ({data, config}) {
+    return {
+      data,
+      config
+    }
   }
 }
