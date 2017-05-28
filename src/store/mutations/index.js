d355c5ab7ee478b06a67bc41b087a3cfc8b543d9
Andrea Zucchini <azga84@gmail.com>
2017-05-29 00:06:25 +0200

    export mutations

--- src/store/mutations/index.js
+++ src/store/mutations/index.js
@@ -1,3 +1,5 @@
 export { switchEffect } from './switchEffect'
 export { toggleDevice } from './toggleDevice'
 export { toggleSidebar } from './toggleSidebar'
+export { setShows } from './setShows'
+export { setCurrentShow } from './setCurrentShow'
