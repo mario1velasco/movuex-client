2ee046216717136b0611ecc4b750cf5b87cf08ef
Andrea Zucchini <azga84@gmail.com>
2017-05-29 00:01:12 +0200

    login component

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="not-found">
+    <a href="http://localhost:3000/api/user/twitter">
+      Twitter
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style lang='scss' src='./Login.scss' scoped></style>
