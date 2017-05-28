1972105ce8a2878567d3f8a62ff3fae954c89966
Andrea Zucchini <azga84@gmail.com>
2017-05-29 00:00:24 +0200

    api url

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  REAL_TIME_URL: '"http://localhost:3000"'
+  REAL_TIME_URL: '"http://localhost:3000"',
+  API_URL: '"http://localhost:3000/api"',
 })
