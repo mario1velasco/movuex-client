217b445085bab37f5a7d024cc3875f21924f9aa8
Andrea Zucchini <azga84@gmail.com>
2017-06-11 17:31:38 +0200

    chat container

---
+++ src/containers/ChatContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <chat
+    :showId="showId"
+  ></chat>
+</template>
+
+<script>
+  import Chat from '../components/Chat/Chat'
+  export default {
+    components: {Chat},
+    name: 'ChatContainer',
+    props: {
+      showId: {
+        require: true
+      }
+    }
+  }
+</script>
+
