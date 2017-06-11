53cb46bf5a549918fd35791a83bf8c20939aeeb6
Andrea Zucchini <azga84@gmail.com>
2017-06-11 17:37:58 +0200

    export service

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,4 @@
 export { HttpService } from './HttpService'
 export { RealTimeService } from './RealTimeService'
-export { TVMazeApiService } from './TVMazeApiService'
+export { ShowsService } from './ShowsService'
 export { AuthService } from './AuthService'
