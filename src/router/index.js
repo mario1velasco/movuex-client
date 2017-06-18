2a8c57c1b2120b5692bdafba64436d6d5af350fd
Andrea Zucchini <azga84@gmail.com>
2017-06-18 13:57:13 +0200

    change components extructure

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Dashboard from '../components/Dashboard/Dashboard'
-import NotFound from '../components/NotFound/NotFound'
-import ShowContainer from '../containers/ShowContainer'
-import ProfileContainer from '../containers/ProfileContainer'
-import ShowListContainer from '../containers/ShowListContainer'
-import GoToDashboard from '../components/GoToDashboard/GoToDashboard'
-import Login from '../components/Login/Login'
+import Dashboard from '../components/Dashboard/Dashboard.vue'
+import App from '../App/App.vue'
+import NotFound from '../components/NotFound/NotFound.vue'
+import ShowContainer from '../containers/ShowContainer.vue'
+import ProfileContainer from '../containers/ProfileContainer.vue'
+import ShowListContainer from '../containers/ShowListContainer.vue'
+import GoToDashboard from '../components/GoToDashboard/GoToDashboard.vue'
+import Login from '../components/Login/Login.vue'
 import { AuthService } from '../services'
 
 Vue.use(Router)
@@ -16,36 +17,46 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Dashboard,
+      component: App,
       beforeEnter: AuthService.requireAuth,
       children: [
         {
           path: '',
-          name: 'showList',
-          component: ShowListContainer,
+          component: Dashboard,
           props: route => {
             const { query } = route
             return query
-          }
-        },
-        {
-          path: '/show/:showId',
-          name: 'show',
-          components: {
-            default: ShowContainer,
-            action: GoToDashboard
           },
-          props: {
-            default: true, // inject show as property
-            action: route => {
-              console.log(route)
+          children: [
+            {
+              path: '',
+              name: 'showList',
+              component: ShowListContainer,
+              props: route => {
+                const { query } = route
+                return query
+              }
+            },
+            {
+              path: '/show/:showId',
+              name: 'show',
+              components: {
+                default: ShowContainer,
+                action: GoToDashboard
+              },
+              props: {
+                default: true, // inject show as property
+                action: route => {
+                  console.log(route)
+                }
+              }
+            },
+            {
+              path: '/user/profile',
+              name: 'user-profile',
+              components: ProfileContainer
             }
-          }
-        },
-        {
-          path: '/user/profile',
-          name: 'user-profile',
-          components: ProfileContainer
+          ]
         }
       ]
     },
