3ce3c367ca8ea5b67f73f8f15dbd91f939581865
Andrea Zucchini <azga84@gmail.com>
2017-06-18 13:56:16 +0200

    improve login component

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,8 +1,29 @@
 <template>
-  <div class="not-found">
-    <a href="/user/twitter">
-      Twitter
-    </a>
+  <div class="wrapper">
+    <div class="card">
+      <div class="card-content">
+        <p class="title">
+          “Movuex”
+        </p>
+        <p class="subtitle">
+          Accede a ver toda la información de tus series favoritas
+        </p>
+      </div>
+      <footer class="card-footer">
+        <p class="card-footer-item">
+      <span>
+        <a href="/user/twitter" class="button twitter">
+          Login with Twitter
+        </a>
+      </span>
+        </p>
+        <p class="card-footer-item">
+          <a href="#" disabled class="button facebook">
+            Login with Facebook
+          </a>
+        </p>
+      </footer>
+    </div>
   </div>
 </template>
 
